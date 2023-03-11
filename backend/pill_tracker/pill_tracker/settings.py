09dad46c7828aca9cdd0f817607ceccea2b9a5b8
damante21 <damante21@gmail.com>
2023-03-11 14:28:18 -0600

    removed secret

--- backend/pill_tracker/pill_tracker/settings.py
+++ backend/pill_tracker/pill_tracker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rrz!j8$7q!x9vwqsniqv+h=y$kh1(kof12f6^u6sjhbsul^+%4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
