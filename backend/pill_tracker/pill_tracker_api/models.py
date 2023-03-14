cdfc5e5598fa1d0867eb36a59c6be00c6031ae2a
tiffanyjoelle <95769342+tiffanyjoelle@users.noreply.github.com>
2023-03-14 18:31:14 -0500

    cleaned up

--- backend/pill_tracker/pill_tracker_api/models.py
+++ backend/pill_tracker/pill_tracker_api/models.py
@@ -4,12 +4,11 @@ from django.conf import settings
 class UserMedication(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     medication_name = models.CharField(max_length=50)
+    medication_notes = models.CharField(max_length=255, null=True, blank=True)
     dosage = models.CharField(max_length=20)
-    # add total pill count for refill tracking? ie. 30, 60, 90
-    # add num of pills/med taken each intake
     rx_number = models.CharField(max_length=50)
     start_date = models.DateField()
-    end_date = models.DateField(null=True, blank=True) # maybe change to refill date
+    refill_date = models.DateField(null=True, blank=True)
     times_per_day = models.PositiveSmallIntegerField(default=1)
     time_of_first_med = models.TimeField()
 
@@ -21,7 +20,6 @@ class MedicationIntake(models.Model):
     date = models.DateField()
     time = models.TimeField()
     taken = models.BooleanField(default=False)
-    # add way to keep track of current pillcount left
 
     def __str__(self):
         return f"{self.medication} on {self.date} at {self.time}"
--- backend/pill_tracker/pill_tracker_api/urls.py
+++ backend/pill_tracker/pill_tracker_api/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    # add med
     path('med', views.UserMedicationAPIView.as_view()),
+    # edit/delete med (also edits intake tracker when frequency/dates are updated)
     path('med/<int:pk>', views.UserMedicationAPIView.as_view()),
-
-     # GET all tracking instances or for a specific date: api/med_tracker?date=2023-03-14
+    # GET all tracking instances or for a specific date: api/med_tracker?date=2023-03-14
+    # or GET all tracking instances 
     path('med_tracker', views.MedicationIntakeAPIView.as_view()),
     # GET specific tracking instance for checkbox
     path('med_tracker/<int:pk>/', views.MedicationIntakeAPIView.as_view()),
