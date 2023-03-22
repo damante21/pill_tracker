86327fce94510a1a03845e9ecb6281711c967dbf
CamrenBell <cjb0132@yahoo.com>
2023-03-22 14:38:59 -0600

    Made register page match styling.

--- frontend/pillTracker/src/pages/Register/Register.jsx
+++ frontend/pillTracker/src/pages/Register/Register.jsx
@@ -1,20 +1,29 @@
 import axios from "axios";
-import React from 'react'
+import React, {useState} from 'react'
 import  { useNavigate  } from 'react-router-dom'
+import { Layout, Form, Button, Input, } from "antd";
+
 
 export default function Register(){
 
+    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
     const navigate = useNavigate();
+    const { Header, Content } = Layout;
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const onFinish = (e) => {
         const data = {
-            'username': e.target['username'].value,
-            'email': e.target['email'].value,
-            'password': e.target['password'].value
+            'username': username,
+            'email': email,
+            'password': password
         }
         sendData(data)
     }
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+      };
 
     const sendData = (data) => {
         axios.post(`http://127.0.0.1:8000/api/register/`, data)
@@ -27,31 +36,65 @@ export default function Register(){
     function loginButtonClick(e){
         e.preventDefault()
         navigate('/Login/')
-      }
+    }
 
+    function handleUsernameChange(e) {
+        setUsername(e.target.value);
+    }
+    
+    function handlePasswordChange(e) {
+        setPassword(e.target.value);
+    }
+
+    function handleEmailChange(e) {
+        setEmail(e.target.value);
+    }
 
 
     return(
-        <div>
-        <form onSubmit={e => {handleSubmit(e)}}>
-            <label>
-                Username:
-                <input type={'text'} name='username' required/><br/>
-            </label>
-            <label>
-                Email:
-                <input type={'text'} name='email' required/><br/>
-            </label>
-            <label>
-                Password:
-                <input type={'password'} name='password' required/><br/>
-            </label>
-            <input type='submit' value={'Submit'}></input>
-        </form>
-        <div>
+        <Layout>
+        <Content>
+            <h2 style={{right: '100px'}}>Register</h2>
+            <Form 
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+            >
+            <Form.Item
+                label='Username'
+                name='username'
+                rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+                <Input onChange={handleUsernameChange}/>
+            </Form.Item>
+            <Form.Item
+                label='Email'
+                name='email'
+                rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+                <Input onChange={handleEmailChange}/>
+            </Form.Item>
+            <Form.Item
+                label='Password'
+                name='password'
+                rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+                <Input.Password onChange={handlePasswordChange}/>
+            </Form.Item>
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type="primary" htmlType="submit">
+                Submit
+                </Button>
+            </Form.Item>
+            </Form><br/>
             <p>Already have an account?</p>
-            <button onClick={loginButtonClick}>Login</button>
-        </div>
-        </div>
+            <Button type="primary" onClick={loginButtonClick}>Login</Button>
+        </Content>
+      </Layout>
     )
 }
