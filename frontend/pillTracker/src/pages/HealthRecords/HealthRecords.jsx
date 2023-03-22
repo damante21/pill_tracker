fb2ba583e2e380f09f0fb59edd409191d7d2c944
reychanchan <reystahl@gmail.com>
2023-03-22 22:47:44 +0800

    add editable health records

--- frontend/pillTracker/src/pages/HealthRecords/HealthRecords.jsx
+++ frontend/pillTracker/src/pages/HealthRecords/HealthRecords.jsx
@@ -1,42 +1,49 @@
-import { theme, Button, Form, Space, Avatar, Divider } from "antd";
+import { theme, Button, Form, Space, Avatar, Divider, Typography } from "antd";
 import ILayout from "../../components/ILayout/ILayout";
 import "./HealthRecords.css";
 import { UserOutlined } from "@ant-design/icons";
+import { useState } from "react";
+
+const { Paragraph } = Typography;
 
 const HealthRecords = () => {
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
 
-  return (
-    <ILayout>
-      <div
-        className="site-layout-content"
-        style={{
-          background: colorBgContainer,
-        }}
-      >
-        <Space direction="vertical" align="center" style={{ width: "100%" }}>
-          <Avatar size={64} icon={<UserOutlined />} />
-          <span>Jack</span>
-        </Space>
-        <Divider />
-        <div className="record-list">
-          <div className="record-item">
-            <h3>Heart rate:</h3>
-            <span>1</span>
-          </div>
-          <div className="record-item">
-            <h3>Blood sugar:</h3>
-            <span>1</span>
-          </div>
-          <div className="record-item">
-            <h3>Allergies:</h3>
-            <span>1</span>
-          </div>
-        </div>
-      </div>
-    </ILayout>
-  );
+    const [username, setUserName] = useState("Jack");
+    const [heartRete, setHeartRete] = useState("1");
+    const [bloodSugar, setBloodSugar] = useState("1");
+    const [Allergies, setAllergies] = useState("1");
+
+    return (
+        <ILayout>
+            <div
+                className="site-layout-content"
+                style={{
+                    background: colorBgContainer,
+                }}>
+                <Space direction="vertical" align="center" style={{ width: "100%" }}>
+                    <Avatar size={64} icon={<UserOutlined />} />
+                    <Paragraph editable={{ onChange: setUserName }}>{username}</Paragraph>
+                </Space>
+                <Divider />
+                <div className="record-list">
+                    <div className="record-item">
+                        <h3>Heart rate:</h3>
+                        <Paragraph editable={{ onChange: setHeartRete }}>{heartRete}</Paragraph>
+                    </div>
+                    <div className="record-item">
+                        <h3>Blood sugar:</h3>
+                        <Paragraph editable={{ onChange: setBloodSugar }}>{bloodSugar}</Paragraph>
+                    </div>
+                    <div className="record-item">
+                        <h3>Allergies:</h3>
+                        <Paragraph editable={{ onChange: setAllergies }}>{Allergies}</Paragraph>
+                    </div>
+                </div>
+            </div>
+        </ILayout>
+    );
 };
 export default HealthRecords;
--- frontend/pillTracker/src/pages/Home/Home.jsx
+++ frontend/pillTracker/src/pages/Home/Home.jsx
@@ -5,83 +5,72 @@ import "./Home.css";
 import ILayout from "../../components/ILayout/ILayout";
 
 const Home = () => {
-  const navigate = useNavigate();
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+    const navigate = useNavigate();
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
 
-  const listData = [
-    {
-      title: "test1",
-    },
-    {
-      title: "test2",
-    },
-    {
-      title: "test1",
-    },
-    {
-      title: "test2",
-    },
-    {
-      title: "test1",
-    },
-    {
-      title: "test2",
-    },
-    {
-      title: "test1",
-    },
-    {
-      title: "test2",
-    },
-  ];
+    const listData = [
+        {
+            title: "test1",
+        },
+        {
+            title: "test2",
+        },
+        {
+            title: "test1",
+        },
+        {
+            title: "test2",
+        },
+        {
+            title: "test1",
+        },
+        {
+            title: "test2",
+        },
+        {
+            title: "test1",
+        },
+        {
+            title: "test2",
+        },
+    ];
 
-  return (
-    <ILayout>
-      <div
-        className="site-layout-content"
-        style={{
-          background: colorBgContainer,
-        }}
-      >
-        <Space
-          direction="vertical"
-          size="small"
-          style={{
-            display: "flex",
-          }}
-        >
-          <h2>Medicine checklist</h2>
-          <List
-            className="med-list"
-            itemLayout="horizontal"
-            dataSource={listData}
-            renderItem={(item, index) => (
-              <List.Item>
-                <List.Item.Meta
-                  avatar={
-                    <Avatar
-                      src={`https://joesch.moe/api/v1/random?key=${index}`}
+    return (
+        <ILayout>
+            <div
+                className="site-layout-content"
+                style={{
+                    background: colorBgContainer,
+                }}>
+                <Space
+                    direction="vertical"
+                    size="small"
+                    style={{
+                        display: "flex",
+                    }}>
+                    <h2>Medicine checklist</h2>
+                    <List
+                        className="med-list"
+                        itemLayout="horizontal"
+                        dataSource={listData}
+                        renderItem={(item, index) => (
+                            <List.Item>
+                                <List.Item.Meta avatar={<Avatar src={`https://joesch.moe/api/v1/random?key=${index}`} />} title={<a href="">{item.title}</a>} description="test111" />
+                            </List.Item>
+                        )}
                     />
-                  }
-                  title={<a href="">{item.title}</a>}
-                  description="test111"
-                />
-              </List.Item>
-            )}
-          />
-          <Button
-            type="primary"
-            onClick={() => {
-              navigate("/home/newMedicine");
-            }}
-          >
-            Add medicine
-          </Button>
-        </Space>
-      </div>
-    </ILayout>
-  );
+                    <Button
+                        type="primary"
+                        onClick={() => {
+                            navigate("/home/newMedicine");
+                        }}>
+                        Add medicine
+                    </Button>
+                </Space>
+            </div>
+        </ILayout>
+    );
 };
 export default Home;
