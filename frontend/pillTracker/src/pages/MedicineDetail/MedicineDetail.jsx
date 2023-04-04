a084a2b822de564957046be0f8a74acf42626297
reychanchan <reystahl@gmail.com>
2023-04-04 10:05:32 +0800

    med detail style

--- frontend/pillTracker/src/pages/MedicineDetail/MedicineDetail.jsx
+++ frontend/pillTracker/src/pages/MedicineDetail/MedicineDetail.jsx
@@ -142,10 +142,10 @@ function MedicineDetail() {
                     <Row>
                         <Col span={12}>
                             <div className="detail-row">
-                            <Button onClick={handleShow}>
+                            <Button type="primary" style={{marginRight:10}} onClick={handleShow}>
                                 Edit Medication
                             </Button> 
-                            <Button onClick={handleDeleteClick}>
+                            <Button type="dashed" danger onClick={handleDeleteClick}>
                                 Delete
                             </Button>
                             </div>
