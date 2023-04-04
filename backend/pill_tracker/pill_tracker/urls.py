d3d109854b71b29dd65d85832064f179dcca1431
James Damante <damante21@gmail.com>
2023-04-03 20:26:52 -0500

    test

--- backend/pill_tracker/pill_tracker/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('pill_tracker_api.urls')),
-    path('nih_api', include('nih_api.urls')),
-]
--- backend/pill_tracker/pill_tracker_api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import UserMedication, MedicationIntake, HealthInformation
-
-# Register your models here.
-admin.site.register(UserMedication)
-admin.site.register(MedicationIntake)
-admin.site.register(HealthInformation)
